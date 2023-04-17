b8347903377b06407f59d1f26cfea94958248ded
Diego Rodriguez <s222883@student.dtu.dk>
2023-04-17 17:33:52 +0200

    frocio

--- featextra.py
+++ featextra.py
@@ -1,8 +1,8 @@
 import tsfresh
 import pandas as pd
 import numpy as np
-def featextra(df,name):
 
+def featextra(df,name):
     feat = tsfresh.extract_features(df[['time',name,'ID']], column_value =name, 
     column_sort='time', column_id='ID', default_fc_parameters = tsfresh.feature_extraction.settings.EfficientFCParameters())
     string = name+'__'
