a0cb4b2c8655e8fddcbf1e2ebcdccaab4bd4eaac
Diego Rodriguez <s222883@student.dtu.dk>
2023-04-20 11:58:05 +0200

    Final script for extracting features

--- featextra.py
+++ featextra.py
@@ -2,9 +2,10 @@ import tsfresh
 import pandas as pd
 import numpy as np
 
-def featextra(df,name):
+def featextra(df,name, n_jobs):
     feat = tsfresh.extract_features(df[['time',name,'ID']], column_value =name, 
-    column_sort='time', column_id='ID', default_fc_parameters = tsfresh.feature_extraction.settings.EfficientFCParameters())
+    column_sort='time', column_id='ID', default_fc_parameters = tsfresh.feature_extraction.settings.EfficientFCParameters(),
+    n_jobs=n_jobs)
     string = name+'__'
     feat.columns = feat.columns.str.replace(string,'')    
     return feat
